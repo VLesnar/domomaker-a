0a05cff478edfe9addc6d096c168c84dcfb66229
Veronica Lesnar <vll4875@rit.edu>
2017-03-30 00:52:04 -0400

    Add homework files

---
+++ server/controllers/Account.js
@@ -0,0 +1,82 @@
+const models = require('../models');
+
+const Account = models.Account;
+
+const loginPage = (req, res) => {
+  res.render('login');
+};
+
+const signupPage = (req, res) => {
+  res.render('signup');
+};
+
+const logout = (req, res) => {
+  res.redirect('/');
+};
+
+const login = (req, res) => {
+  const request = req;
+  const response = res;
+
+  const username = `${request.body.username}`;
+  const password = `${request.body.pass}`;
+
+  if (!username || !password) {
+    return response.status(400).json({ error: 'RAWR! All fields are required!' });
+  }
+
+  return Account.AccountModel.authenticate(username, password, (err, account) => {
+    if (err || !account) {
+      return response.status(401).json({ error: 'Wrong username or password.' });
+    }
+
+    return response.json({ redirect: '/maker' });
+  });
+};
+
+const signup = (req, res) => {
+  const request = req;
+  const response = res;
+
+  request.body.username = `${request.body.username}`;
+  request.body.pass = `${request.body.pass}`;
+  request.body.pass2 = `${request.body.pass2}`;
+
+  if (!request.body.username || !request.body.pass || !request.body.pass2) {
+    return response.status(400).json({ error: 'RAWR! All fields are required!' });
+  }
+
+  if (request.body.pass !== request.body.pass2) {
+    return response.status(400).json({ error: 'RAWR! Passwords do not match!' });
+  }
+
+  return Account.AccountModel.generateHash(request.body.pass, (salt, hash) => {
+    const accountData = {
+      username: request.body.username,
+      salt,
+      password: hash,
+    };
+
+    const newAccount = new Account.AccountModel(accountData);
+
+    const savePromise = newAccount.save();
+
+    savePromise.then(() => response.json({ redirect: '/maker' }));
+
+    savePromise.catch((err) => {
+      console.log(err);
+
+      if (err.code === 11000) {
+        return response.status(400).json({ error: 'Username already in use.' });
+      }
+
+      return response.status(400).json({ error: 'An error occurred.' });
+    });
+  });
+};
+
+module.exports.loginPage = loginPage;
+module.exports.login = login;
+module.exports.logout = logout;
+module.exports.signupPage = signupPage;
+module.exports.signup = signup;
